bc3a3703cf3c4cfafecdf39920a40a34f06abedb
Luis Zancanela <luisczdidi@gmail.com>
2024-11-19 19:06:20 -0300

    feat: corrige propagação do clique que chama página produto

--- src/components/Menu/MenuProdutos.jsx
+++ src/components/Menu/MenuProdutos.jsx
@@ -37,53 +37,59 @@ const MenuProdutos = ({ produtos }) => {
       {produtos.map((produto, index) => (
         <div key={index}>
           <div className="col d-flex flex-column mx-auto">
-            <div 
+            <div
               className="d-flex align-items-center align-items-md-start align-items-xl-center"
-              onClick={() => toPaginaProduto(produto.id)}>
+              onClick={() => toPaginaProduto(produto.id)}
+            >
               <div className="bs-icon-xl bs-icon-circle bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center col-3 pe-2">
-                  <img
-                    className="rounded w-100"
-                    src={produto.imagem ? produto.imagem : logo}
-                    alt={`foto ${produto.nome}`}
-                  />
+                <img
+                  className="rounded w-100"
+                  src={produto.imagem ? produto.imagem : logo}
+                  alt={`foto ${produto.nome}`}
+                />
               </div>
               <div className="d-flex flex-column col-9">
-                  <div className="d-flex">
-                    <div className="col-8 flex-shrink-1">
-                      <h4>{produto.nome}</h4>
-                      <p className="fw-light text-muted text-truncate">
-                        {produto.descricao}
-                      </p>
-                    </div>
-                    <div className="d-flex flex-column col-4 flex-grow-1 align-items-end">
-                      {produto.valorDesconto && produto.valorDesconto >= 0 ? (
-                        <>
-                          <p className="fw-light text-danger text-decoration-line-through mb-0">
-                            {produto.valorOriginal.toLocaleString('pt-BR', {
-                              style: 'currency',
-                              currency: 'BRL',
-                            })}
-                          </p>
-                          <p className="fw-semibold fs-2 my-0">
-                            {(
-                              produto.valorOriginal - produto.valorDesconto
-                            ).toLocaleString('pt-BR', {
-                              style: 'currency',
-                              currency: 'BRL',
-                            })}
-                          </p>
-                        </>
-                      ) : (
-                        <p className="fw-semibold fs-2">
+                <div className="d-flex">
+                  <div className="col-8 flex-shrink-1">
+                    <h4>{produto.nome}</h4>
+                    <p className="fw-light text-muted text-truncate">
+                      {produto.descricao}
+                    </p>
+                  </div>
+                  <div className="d-flex flex-column col-4 flex-grow-1 align-items-end">
+                    {produto.valorDesconto && produto.valorDesconto >= 0 ? (
+                      <>
+                        <p className="fw-light text-danger text-decoration-line-through mb-0">
                           {produto.valorOriginal.toLocaleString('pt-BR', {
                             style: 'currency',
                             currency: 'BRL',
                           })}
                         </p>
-                      )}
-                    </div>
+                        <p className="fw-semibold fs-2 my-0">
+                          {(
+                            produto.valorOriginal - produto.valorDesconto
+                          ).toLocaleString('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL',
+                          })}
+                        </p>
+                      </>
+                    ) : (
+                      <p className="fw-semibold fs-2">
+                        {produto.valorOriginal.toLocaleString('pt-BR', {
+                          style: 'currency',
+                          currency: 'BRL',
+                        })}
+                      </p>
+                    )}
                   </div>
-                <div className="input-group">
+                </div>
+                <div
+                  className="input-group"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
                   <span className="input-group-text">Quantidade</span>
                   <button
                     className="btn btn-light"
@@ -126,7 +132,8 @@ const MenuProdutos = ({ produtos }) => {
                   href="#"
                   type="button"
                   className="btn btn-primary flex-grow-1 flex-shrink-0 flex-fill mt-2"
-                  onClick={() => {
+                  onClick={(e) => {
+                    e.stopPropagation();
                     adicionarProdutoAoCarrinho(
                       produto,
                       quantidades[produto.id]
